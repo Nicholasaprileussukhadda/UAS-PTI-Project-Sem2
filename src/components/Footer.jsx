25bc5b464cd3488ed4d3f9c84c8d720a48ca32ba
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-05-09 22:10:02 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import "../styles.css";
+import "./styles.css";
 
 function Footer(){
     return (
         <div className="container">
             <div className="container mt-5 fluid" id="Footer1">
                 <div className="row">
+                    <div className="col-sm-3" id="Alamat">
+                        <p>Jalan Scientia Boulevard Gading, Curug Sangereng, Serpong, Kabupaten Tangerang, Banten 15810.</p>
+                        <br />
+                        <p>Telp: (021) 54220808</p>
+                    </div>
                     <div className="col-sm-6">
-                        //Alamat dan no telp//
                     </div>
                     <div className="col-sm-3">
                         //logo UMN//
