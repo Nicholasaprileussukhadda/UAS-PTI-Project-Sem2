3a18842c1128f35191fdb97e92e8ae56709c255a
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-05-09 22:04:19 +0700

    Update IdGambar.jsx

--- src/components/IdGambar.jsx
+++ src/components/IdGambar.jsx
@@ -4,16 +4,16 @@ import GambarKota from "./GambarKota";
 function IdGambar() {
   return (
     <div>
-      <GambarKota image="Padang.jpg" id="action-1" />
-      <GambarKota image="medan.png" id="action-2" />
-      <GambarKota image="pontianak.jpg" id="action-3" />
-      <GambarKota image="samarinda.jpeg" id="action-4" />
-      <GambarKota image="makassar.jpg" id="action-5" />
-      <GambarKota image="manado.jpeg" id="action-6" />
-      <GambarKota image="semarang.jpeg" id="action-7" />
-      <GambarKota image="denpasar.jpg" id="action-8" />
-      <GambarKota image="jayapura.jpg" id="action-9" />
-      <GambarKota image="sorong.jpg" id="action-10" />
+      <GambarKota image="../gambar/Padang.jpg" id="action-1" />
+      <GambarKota image="../gambar/medan.png" id="action-2" />
+      <GambarKota image="../gambar/pontianak.jpg" id="action-3" />
+      <GambarKota image="../gambar/samarinda.jpeg" id="action-4" />
+      <GambarKota image="../gambar/makassar.jpg" id="action-5" />
+      <GambarKota image="../gambar/manado.jpeg" id="action-6" />
+      <GambarKota image="../gambar/semarang.jpeg" id="action-7" />
+      <GambarKota image="../gambar/denpasar.jpg" id="action-8" />
+      <GambarKota image="../gambar/jayapura.jpg" id="action-9" />
+      <GambarKota image="../gambar/sorong.jpg" id="action-10" />
     </div>
   );
 }
