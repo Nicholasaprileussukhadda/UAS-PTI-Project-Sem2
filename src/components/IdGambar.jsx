7ea2080bd9ef6d1b68bd9ce4851c16d0600a1f5d
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-05-09 22:03:30 +0700

    Update IdGambar.jsx

--- src/components/IdGambar.jsx
+++ src/components/IdGambar.jsx
@@ -5,15 +5,15 @@ function IdGambar() {
   return (
     <div>
       <GambarKota image="Padang.jpg" id="action-1" />
-      <GambarKota image="bandung.jpg" id="action-2" />
-      <GambarKota image="surabaya.jpg" id="action-3" />
-      <GambarKota image="surabaya.jpg" id="action-4" />
-      <GambarKota image="surabaya.jpg" id="action-5" />
-      <GambarKota image="surabaya.jpg" id="action-6" />
-      <GambarKota image="surabaya.jpg" id="action-7" />
-      <GambarKota image="surabaya.jpg" id="action-8" />
-      <GambarKota image="surabaya.jpg" id="action-9" />
-      <GambarKota image="surabaya.jpg" id="action-10" />
+      <GambarKota image="medan.png" id="action-2" />
+      <GambarKota image="pontianak.jpg" id="action-3" />
+      <GambarKota image="samarinda.jpeg" id="action-4" />
+      <GambarKota image="makassar.jpg" id="action-5" />
+      <GambarKota image="manado.jpeg" id="action-6" />
+      <GambarKota image="semarang.jpeg" id="action-7" />
+      <GambarKota image="denpasar.jpg" id="action-8" />
+      <GambarKota image="jayapura.jpg" id="action-9" />
+      <GambarKota image="sorong.jpg" id="action-10" />
     </div>
   );
 }
