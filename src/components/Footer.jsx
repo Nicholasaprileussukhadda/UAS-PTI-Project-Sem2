059a502e51fc4eb80139f73ed403d0df5897ba84
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-05-09 20:03:36 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles.css";
+
+function Footer(){
+    return (
+        <div className="container">
+            <div className="container mt-5 fluid" id="Footer1">
+                <div className="row">
+                    <div className="col-sm-6">
+                        //Alamat dan no telp//
+                    </div>
+                    <div className="col-sm-3">
+                        //logo UMN//
+                    </div>
+                </div>
+            </div>
+        </div>  
+    );
+}
+export default Footer;
